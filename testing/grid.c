4e61ebf4691dcf6a2cae3cdef0b45afc54bf343a
Arsela Bita <arselabita@gmail.com>
2026-04-08 19:26:40 +0200

    created diff maps and i think its working the map valid

--- testing/grid.c
+++ testing/grid.c
@@ -90,8 +90,8 @@ int get_validation(char **grid, int height)
 {
     int x;
     int y;
-    int player_x = 0;
-    int player_y = 0;
+    int player_x;
+    int player_y;
     char **grid_copy;
 
     y = 0;
