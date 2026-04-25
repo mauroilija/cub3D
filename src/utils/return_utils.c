72e29e44982b4d17410a217fca1d21c0248e5276
Arsela Bita <arselabita@gmail.com>
2026-04-25 14:18:46 +0200

    Removed an unused function

--- src/utils/return_utils.c
+++ src/utils/return_utils.c
@@ -6,7 +6,7 @@
 /*   By: abita <abita@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 17:29:49 by abita             #+#    #+#             */
-/*   Updated: 2026/04/24 21:08:06 by abita            ###   ########.fr       */
+/*   Updated: 2026/04/25 14:15:05 by abita            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,4 @@ void	print_error(const char *msg)
 	write(STDERR_FILENO, RED, 5);
 	write(STDERR_FILENO, msg, ft_strlen(msg));
 	write(STDERR_FILENO, RED, 5);
-}
-
-void	print_pass(const char *msg)
-{
-	write(STDERR_FILENO, GREEN, 5);
-	write(STDERR_FILENO, msg, ft_strlen(msg));
-	write(STDERR_FILENO, GREEN, 5);
 }
