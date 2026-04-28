40027ddd1f029e928998f630f8cb91976e36a988
Mauro <mauroilija2003@gmail.com>
2026-04-28 10:47:56 +0200

    left and right movement properly done

--- src/mlx/movement/movement.c
+++ src/mlx/movement/movement.c
@@ -6,7 +6,7 @@
 /*   By: milija-h <milija-h@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/15 21:19:46 by milija-h          #+#    #+#             */
-/*   Updated: 2026/04/26 19:37:00 by milija-h         ###   ########.fr       */
+/*   Updated: 2026/04/28 10:45:43 by milija-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,19 @@ void	move_forward_backward(t_player *p, double move_speed, char **map,
 		p->pos_y = new_y;
 }
 
-//turn player left of right
+void	move_left_right(t_player *p, double move_speed, char **map, int dir)
+{
+	double	new_x;
+	double	new_y;
+
+	new_x = p->pos_x + p->dir_y * move_speed * dir;
+	new_y = p->pos_y - p->dir_x * move_speed * dir;
+	if (map[(int)p->pos_y][(int)new_x] != '1')
+		p->pos_x = new_x;
+	if (map[(int)new_y][(int)p->pos_x] != '1')
+		p->pos_y = new_y;
+}
+
 void	rotate_player(t_player *p, double rot_speed, int dir)
 {
 	double	old_dir_x;
@@ -67,4 +79,8 @@ void	update_player(t_player *p, char **map, double frame_time)
 		rotate_player(p, p->rot_speed, -1);
 	if (p->key_right)
 		rotate_player(p, p->rot_speed, +1);
+	if (p->a_key)
+		move_left_right(p, p->move_speed, map, 1);
+	if (p->d_key)
+		move_left_right(p, p->move_speed, map, -1);
 }
